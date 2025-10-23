ad9ca4469e573f08db5761c2457a9ebaa0076c8f
ryan2135 <ryan_djr@yahoo.com>
2025-10-23 11:29:23 -0700

    Starting mongoose logic

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,17 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import mongoose from 'mongoose';
 
 const app = express();
+
+const mongoDB = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/local_library';
+
+mongoose.connect(mongoDB)
+.then(()=>console.log('Connected to MongoDB'))
+.catch(err=>console.error(err));
+
+
 
 // Resolve __dirname since we're using ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -15,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Example route (we’ll replace later with real ones)
 app.get("/", (req, res) => {
-  res.send("Local Library backend is running!");
+  res.send("Local Library backend is running!!");
 });
 
 // Export the app for bin/www to import
