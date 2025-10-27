c2ed23eb4c4fa1841a5205876405136be289fbc9
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-10-27 18:23:37 +0000

    Tweaking ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import "./styles/App.css";
 import Sidebar from "./components/layouts/Sidebar.jsx";
 import Home from "./Home.jsx";
@@ -12,27 +11,19 @@ import Category3 from "./Category3.jsx";
 function App() {
   return (
     <Router>
-      <div className="container">
-        <PanelGroup direction="horizontal">
-          {/* Use fixed width instead of defaultSize % */}
-          <Panel defaultSize={240} minSize={240} maxSize={240}>
-            <Sidebar />
-          </Panel>
+      <div className="app-layout">
+        {/* Fixed sidebar */}
+        <Sidebar />
 
-          {/* Remove resize handle since we’re fixing width */}
-          {/* <PanelResizeHandle className="resizer" /> */}
-
-          <Panel>
-            <div className="content">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/category1" element={<Category1 />} />
-                <Route path="/category2" element={<Category2 />} />
-                <Route path="/category3" element={<Category3 />} />
-              </Routes>
-            </div>
-          </Panel>
-        </PanelGroup>
+        {/* Scrollable right content area */}
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category1" element={<Category1 />} />
+            <Route path="/category2" element={<Category2 />} />
+            <Route path="/category3" element={<Category3 />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
