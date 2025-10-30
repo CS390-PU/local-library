2244e59b9e1b77ec728671b586ac9884ddd0ca4e
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-10-30 23:12:46 +0000

    Changed file structure

--- frontend/src/Category1.jsx
+++
@@ -1,5 +0,0 @@
-// src/Category1.jsx
-import React from "react";
-export default function Category1() {
-  return <h2>All Books</h2>;
-}
--- frontend/src/Category2.jsx
+++
@@ -1,5 +0,0 @@
-// src/Category2.jsx
-import React from "react";
-export default function Category2() {
-  return <h2>All Authors</h2>;
-}
